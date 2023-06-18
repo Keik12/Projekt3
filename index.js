fd1c380af916bf1d74ec8c2fa2669f3a72275bac
Emil Kubela <emilkubela4@gmail.com>
2023-06-18 23:45:10 +0200

    Initial Commit

--- index.js
+++ index.js
@@ -1,64 +1,112 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
 const revenueForm = document.querySelector("#revenueForm");
 const revenueName = document.querySelector("#revenueName");
 const revenueAmount = document.querySelector("#revenueAmount");
+const revenueList = document.querySelector("#revenueList");
+
 const spendingForm = document.querySelector("#spendingForm");
 const spendingName = document.querySelector("#spendingName");
 const spendingAmount = document.querySelector("#spendingAmount");
-import { v4 as uuidv4 } from "https://jspm.dev/uuid";
-const revenueDetails = document.querySelector("#revenueDetails");
-const spendingDetails = document.querySelector("#spendingDetails");
-const revenueList = document.querySelector("#revenueList");
 const spendingList = document.querySelector("#spendingList");
-const item = document.querySelector("#item");
-const revenueSum = document.querySelector("#revenueSum");
-const spendingSum = document.querySelector("#spendingSum");
 
-let revenueTable = [];
+const balance = document.getElementById("balance");
 
-let spendingTable = [];
+let revenueArray = [];
+
+let spendingArray = [];
 
 revenueForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("poszlo");
-  let nameValue1 = revenueName.value;
-  let amountValue1 = revenueAmount.value;
+
+  const nameValue = revenueName.value;
+  const amountValue = revenueAmount.value;
+
   revenueName.value = "";
   revenueAmount.value = "";
-  const li = appendList(nameValue1, amountValue1, revenueList);
-  console.log(li.id);
-  revenueTable.push({ id: li.id, name: nameValue1, amount: amountValue1 });
-  const calculate = calcSum(revenueTable);
-  console.log(calculate);
-  const showSum = document.getElementById("revenueSum");
-  showSum.textContent = calculate;
+
+  const li = appendList(nameValue, amountValue, revenueList, "REVENUE");
+
+  revenueArray.push({ id: li.id, name: nameValue, amount: amountValue });
+
+  document.getElementById("revenueSum").textContent = calcSum(revenueArray);
+  setBalance();
 });
 
 spendingForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("poszlo2");
-  let nameValue2 = spendingName.value;
-  let amountValue2 = spendingAmount.value;
+
+  const nameValue = spendingName.value;
+  const amountValue = spendingAmount.value;
+
   spendingName.value = "";
   spendingAmount.value = "";
-  const li = appendList(nameValue2, amountValue2, spendingList);
-  console.log(li.id);
-  spendingTable.push({ id: li.id, name: nameValue2, amount: amountValue2 });
-  const calculate = calcSum(spendingTable);
-  console.log(calculate);
-  const showSum = document.getElementById("spendingSum");
-  showSum.textContent = calculate;
+
+  const li = appendList(nameValue, amountValue, spendingList, "SPENDING");
+
+  spendingArray.push({ id: li.id, name: nameValue, amount: amountValue });
+
+  document.getElementById("spendingSum").textContent = calcSum(spendingArray);
+  setBalance();
 });
 
-// function spanList(name, amount) {
-//   const spanName = document.createElement("span");
-//   spanName.textContent = name;
-//   spanName.id = `${li.id}-name`;
-//   const spanAmount = document.createElement("span");
-//   spanAmount.textContent = amount;
-//   spanAmount.id = `${li.id}-amount`;
-// }
+function editElement(id, mode) {
+  let currentElement;
 
-function appendList(name, amount, container) {
+  if (mode === "SPENDING") {
+    currentElement = spendingArray.find((element) => element.id === id);
+  } else {
+    currentElement = revenueArray.find((element) => element.id === id);
+  }
+
+  if (!currentElement) {
+    return;
+  }
+
+  const name = window.prompt("Nowa nazwa", currentElement.name);
+  const amount = window.prompt("Nowa wartosc", currentElement.amount);
+
+  if (!name || !amount) {
+    return;
+  }
+
+  document.getElementById(`${id}-name`).innerText = name;
+  document.getElementById(`${id}-amount`).innerText = amount;
+
+  if (mode === "SPENDING") {
+    spendingArray = spendingArray.map((el) => {
+      if (el.id === id) {
+        return {
+          ...el,
+          name,
+          amount,
+        };
+      } else {
+        return el;
+      }
+    });
+
+    document.getElementById("spendingSum").innerText = calcSum(spendingArray);
+  } else {
+    revenueArray = revenueArray.map((el) => {
+      if (el.id === id) {
+        return {
+          ...el,
+          name,
+          amount,
+        };
+      } else {
+        return el;
+      }
+    });
+
+    document.getElementById("revenueSum").innerText = calcSum(revenueArray);
+  }
+
+  setBalance();
+}
+
+function appendList(name, amount, container, mode) {
   let li = document.createElement("li");
   li.id = uuidv4();
 
@@ -70,71 +118,55 @@ function appendList(name, amount, container) {
   spanAmount.textContent = amount;
   spanAmount.id = `${li.id}-amount`;
 
+  const btnEdit = document.createElement("button");
+  btnEdit.textContent = "edytuj";
+
+  const btnDelete = document.createElement("button");
+  btnDelete.textContent = "usuń";
+
   li.appendChild(spanName);
   li.appendChild(spanAmount);
-  container.appendChild(li);
-  let btnEdit = document.createElement("button");
-  btnEdit.textContent = "edytuj";
   li.appendChild(btnEdit);
-  let btnDelete = document.createElement("button");
-  btnDelete.textContent = "usuń";
   li.appendChild(btnDelete);
 
-  btnEdit.addEventListener("click", (event) => {
-    document.getElementById(`${li.id}-amount`).contentEditable = true;
-    document.getElementById(`${li.id}-name`).contentEditable = true;
+  container.appendChild(li);
+
+  btnEdit.addEventListener("click", () => {
+    editElement(li.id, mode);
   });
 
   btnDelete.addEventListener("click", (event) => {
     document.getElementById(li.id).remove();
+    if (mode === "SPENDING") {
+      spendingArray = spendingArray.filter((el) => el.id !== li.id);
+      document.getElementById("spendingSum").innerText = calcSum(spendingArray);
+    } else {
+      revenueArray = revenueArray.filter((el) => el.id !== li.id);
+      document.getElementById("revenueSum").innerText = calcSum(revenueArray);
+    }
+    setBalance();
   });
-
-  spanAmount.addEventListener("input", (event) => {
-    revenueTable = revenueTable.map((item) => {
-      return item.id === li.id
-        ? { ...item, amount: event.target.innerText }
-        : item;
-    });
-    console.log(revenueTable);
-  });
-
-  spanName.addEventListener("input", (event) => {
-    revenueTable = revenueTable.map((item) => {
-      return item.id === li.id
-        ? { ...item, name: event.target.innerText }
-        : item;
-    });
-    console.log(revenueTable);
-  });
-
-  function editElement(id) {
-    currentElement = revenues.find((element) => element.id === id);
-
-    const name = window.prompt("Nowa nazwa", currentElement.name);
-    const amount = window.prompt("Nowa wartosc", currentElement.amount);
-
-    currentElement.name = name;
-    currentElement.amount = amount;
-  }
-
-  editButton.addEventListener("click", () => {
-    editElement(id);
-  });
-
-  // const editSpan = (span) = > {
-  // span.addEventListener
-  // editSpan(spanAmount)
-  // editSpan(spanName)
-  // editSpan(spanName, revenueTable,  "name")
-  // editSpan(spanAmount, revenueTable,  "amount")
 
   return li;
 }
 
 function calcSum(array) {
-  const sum = array.reduce((acc, prev) => {
-    console.log(Number(acc.amount), Number(prev.amount), acc);
+  return array.reduce((acc, prev) => {
     return acc + Number(prev.amount);
   }, 0);
-  return sum;
+}
+
+function setBalance() {
+  const revenueSum = Number(document.getElementById("revenueSum").innerText);
+  const spendingSum = Number(document.getElementById("spendingSum").innerText);
+
+  if (revenueSum > spendingSum) {
+    balance.innerText = `Możesz jeszcze wydać ${revenueSum - spendingSum} zł`;
+  } else if (spendingSum > revenueSum) {
+    balance.innerText = `Bilans jest ujemny.Jesteś na minusie ${
+      spendingSum - revenueSum
+    } zł`;
+  } else {
+    balance.innerText = `Bilans wynosi zero.`;
+  }
 }
